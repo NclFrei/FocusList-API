46e2bba9e29a534df0ef5302bf065e440af3a591
NclFrei <nicollasfreieleoterio@gmail.com>
2025-02-06 02:33:36 -0300

    Criando metodos no DAL

---
+++ FocusList.Shared.Dados/Banco/DAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusList.Shared.Dados.Banco;
+
+public class DAL <T> where T : class
+{
+    private readonly FocusListContext context;
+
+    public DAL(FocusListContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<T> listar()
+    {
+        return context.Set<T>().ToList();
+    }
+
+    public void Adicionar(T objeto)
+    {
+        context.Set<T>().Add(objeto);
+        context.SaveChanges();
+    }
+
+    public void Atualizar(T objeto)
+    {
+        context.Set<T>().Update(objeto);
+        context.SaveChanges();
+    }
+
+    public void Deletar(T objeto)
+    {
+        context.Set<T>().Remove(objeto);
+        context.SaveChanges();
+    }
+
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
+    }
+}
