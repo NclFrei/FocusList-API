f4413a1617cc3a669b6ba99c2e896b947fa691ca
NclFrei <nicollasfreieleoterio@gmail.com>
2025-02-06 02:01:36 -0300

    Adicionar arquivos de projeto.

---
+++ FocusList.Shared.Modelos/Entidades/Tarefas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusList.Shared.Modelos.Entidades;
+
+public class Tarefas
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string? Status { get; set; }
+    public int? AmbienteId { get; set; }
+    public int? UsuarioId { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataFim { get; set; }
+    public string? Icone { get; set; }
+
+    public void ExibirTarefa()
+    {
+        Console.WriteLine($"Nome: {Nome}" +
+            $"Descrição: {Descricao}" +
+            $"Status: {Status}" +
+            $"Data inicio: {DataInicio}" +
+            $"Data fim: {DataFim}" +
+            $"Icone: {Icone}");
+    }
+}
+
+
+
