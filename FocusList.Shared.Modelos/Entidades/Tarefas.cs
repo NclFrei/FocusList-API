14e3325c12e65d940587c596375628101c847e97
NclFrei <nicollasfreieleoterio@gmail.com>
2025-02-06 02:09:12 -0300

    Instalando pacotes

--- FocusList.Shared.Modelos/Entidades/Tarefas.cs
+++ FocusList.Shared.Modelos/Entidades/Tarefas.cs
@@ -8,11 +8,11 @@ namespace FocusList.Shared.Modelos.Entidades;
 
 public class Tarefas
 {
-    public int Id { get; set; }
+    public int IdTarefa { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
     public string? Status { get; set; }
-    public int? AmbienteId { get; set; }
+    public int? IdAmbiente { get; set; }
     public int? UsuarioId { get; set; }
     public DateTime DataInicio { get; set; }
     public DateTime DataFim { get; set; }
